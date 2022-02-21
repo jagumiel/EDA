d5566bbcde4b7a2fc34a973a66a9c774995ce3b7
Jose Ángel Gumiel <jagumiel001@gmail.com>
2022-02-21 22:06:02 +0100

    New functions.

--- lab1/src/lab1/ListaPeliculasPrincipal.java
+++ lab1/src/lab1/ListaPeliculasPrincipal.java
@@ -43,6 +43,85 @@ public class ListaPeliculasPrincipal {
 		}
 	}
 	
+	
+	
+
+	/*public void anadirPeliculaOrdenada(Pelicula pPelicula) 
+	{
+		if((pPelicula!=null)&&(!estaPelicula(pPelicula))){
+			this.getLista().push(pPelicula);
+			this.getLista().sort(function(a,b) {return a-b});
+			return this.getLista();
+		}
+		else{
+			System.out.println("la pelicula introducida no es valida.");
+		}
+	}*/
+
+	//En el caso de que funcione se aplica tambien al resto de listas.
+	public void anadirPeliculaOrdenada(Pelicula pPelicula) 
+	{
+		if((pPelicula!=null)&&(!estaPelicula(pPelicula)))
+		{
+			String titulo=pPelicula.getTitulo();
+			int longitud=this.getLista().size();
+			int i=0;
+			boolean ordenado=false;
+			while (!ordenado) 
+			{
+				if(longitud>0)
+				{
+					i=longitud/2;
+					String peliAux=this.getLista().get(i).getTitulo();
+					int resul=titulo.compareTo(peliAux);
+					if (resul>0)
+					{
+						int aux=longitud-i;
+						aux=aux/2;
+						i=i+aux;
+					}
+					else
+					{
+						int aux=longitud-i;
+						aux=aux/2;
+						i=i-aux;
+					}
+					if((this.getLista().get(i-1).getTitulo().compareTo(titulo)<0) &&(this.getLista().get(i).getTitulo().compareTo(titulo)>0))
+					{
+						desplazarDerecha(i, longitud);
+						this.getLista().add(i, pPelicula);
+						ordenado=true;
+					}
+				}
+				else
+				{
+					this.getLista().add(pPelicula);
+					ordenado=true;
+				}
+			}
+		}
+		else
+		{
+			System.out.println("la pelicula introducida no es valida.");
+		}
+	}
+	
+	public void desplazarDerecha(int i, int longitud)
+	{
+		Pelicula aux;
+		Pelicula aux2;
+		while (i!=longitud+1)
+		{
+			aux=this.getLista().get(i);
+			aux2=this.getLista().get(i+1);
+			this.getLista().add(i+1, aux);
+			i=i+1;
+			aux=this.getLista().get(i);
+			this.getLista().add(i+1, aux2);
+		}
+	}
+
+	
 	public void eliminarPelicula(Pelicula pPelicula){
 		if((pPelicula!=null)&&(estaPelicula(pPelicula))){
 			this.getLista().remove(pPelicula);
